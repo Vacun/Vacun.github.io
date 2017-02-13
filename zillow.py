51fb496d513db8f04c695a41893f5e681e54f45e
Vacun <vahe.voskerchyan@gmail.com>
2017-02-13 00:08:11 -0500

    working zillow and scrapy/trulia

--- zillow.py
+++ zillow.py
@@ -6,7 +6,8 @@ import csv
 
 driver = webdriver.Chrome()
 driver.get('http://www.zillow.com/homes/for_sale/New-York-NY_rb/?fromHomePage=true&shouldFireSellPageImplicitClaimGA=false&fromHomePageTab=buy')
-
+driver.maximize_window()
+#driver.manage().window().maximize()
 # successful clicking in a website
 # driver.get('https://en.wikipedia.org/wiki/Armenia')
 # driver.find_elements_by_xpath('//*[@id="mw-content-text"]/p[17]/a[1]')[0].click()
@@ -20,17 +21,18 @@ page = 0
 count = 0
 
 csv_file = open('listings.csv', 'wb')
-writer = csv.writer(csv_file)
-writer.writerow(['address', 'price', 'zprice','bedrooms','bathrooms','floor_size','est_mortgage','latitude', 'longitude','FEATURES','APPLIANCES INCLUDED','OTHER','ROOM TYPES','CONSTRUCTION','FACTS','ADDITIONAL FEATURES'])
-
+fieldnames=['address','price','floor_size','bathrooms','bedrooms','est_mortgage', 'zprice','latitude','longitude','FACTS','FEATURES','APPLIANCES INCLUDED','ROOM TYPES','CONSTRUCTION','ADDITIONAL FEATURES','OTHER']
+writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+#writer.writerow(['bathrooms', 'price', 'bedrooms', 'longitude', 'est_mortgage', 'zprice', 'address', 'latitude', 'floor_size','FEATURES','APPLIANCES INCLUDED','OTHER','CONSTRUCTION','ROOM TYPES','ADDITIONAL FEATURES'])
+writer.writeheader()
 for i in range(last_page):
 	page =page + 1
 	n = 0
 	listings = driver.find_elements_by_xpath('//*[@id="search-results"]/ul/li')
-
+		
 	for i  in range(len(listings)):
 		n=i+1
-		
+
 		listing_dict = {}
 
 		print 'Scraping the listing number {0} on page {1}, the count is {2}'.format(n,page,count)
@@ -57,48 +59,46 @@ for i in range(last_page):
 
 			# now the fun part of scraping itself
 			time.sleep(2)
-
-
 			# driver.find_element_by_xpath('//section[@class="zsg-content-section"]')
 			try:
 				address = driver.find_element_by_xpath('//header/h1').text
-				#print address
+				print address
 				listing_dict['address'] = address
 			except:
 				print 'No Address'
 			try:
 				price = driver.find_element_by_xpath('//div[@class="estimates"]/div[2]').text
-				#print price
+				print price
 				listing_dict['price'] = price
 			except:
 				print 'No Price'
 			try:
 				zprice = driver.find_element_by_xpath('//div[@class="estimates"]/div[3]/span[2]').text
-				#print zprice
+				print zprice
 				listing_dict['zprice'] = zprice
 			except:
-				print 'No Zprice'
+				print "No Zprice"
 			try:				
 				bedrooms = driver.find_element_by_xpath('//header/h3/span[2]').text
-				#print bedrooms
+				print bedrooms
 				listing_dict['bedrooms'] = bedrooms
 			except:
 				print "No Bedrooms"
 			try:
 				bathrooms = driver.find_element_by_xpath('//header/h3/span[4]').text
-				#print bathrooms
+				print bathrooms
 				listing_dict['bathrooms'] = bathrooms
 			except:
 				print 'No Bathrooms'	
 			try:	
 				floor_size = driver.find_element_by_xpath('//header/h3/span[6]').text
-				#print floor_size
+				print floor_size
 				listing_dict['floor_size'] = floor_size
 			except:
 				print 'No floor size'
 			try:
 				est_mortgage = driver.find_element_by_xpath('//div[@class="loan-calculator-container"]//span[1]/span').text
-				#print est_mortgage
+				print est_mortgage
 				listing_dict['est_mortgage'] = est_mortgage
 			except:
 				print 'No loan Calculater'
@@ -112,20 +112,32 @@ for i in range(last_page):
 				for things in features_content_list:
 					features_content = features_content+'\n'+things.text
 				facts_dict[features] = features_content
-			print facts_dict.keys()
-
-			writer.writerow(listing_dict.values())
+			listing_dict.update(facts_dict)
+			try:
+				writer.writerow(listing_dict)
+				print 'DATA CAPTURED'
+			except:
+				print 'It GOT AWAY'
 
 			driver.find_element_by_xpath('//*[@id="actionBar"]/ul[2]/li[2]/button/span').click()
 
 			try:
+				driver.find_element_by_xpath('//*[@id="actionBar"]/ul[2]/li[2]/button/span').click()
+			except:
+				try:
+					driver.switch_to_frame("register_content")
+					driver.find_element_by_xpath('//div[@id="register_content"]/a').click()
+					driver.switch_to_default_content()
+				except:
+					continue
+		except:
+			try:
+				driver.switch_to_frame("register_content")
 				driver.find_element_by_xpath('//div[@id="register_content"]/a').click()
+				driver.switch_to_default_content()
 			except:
 				continue
-		
 
-		except:
-			continue
 		
 	driver.find_element_by_xpath('//div[@id="search-pagination-wrapper"]//li[@class="zsg-pagination-next"]').click()
 	time.sleep(2)
